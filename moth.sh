fdf0ca2482600ef80f2377f65118b4d8a152c2f9
freitzzz <1160907@isep.ipp.pt>
2018-09-30 21:47:51 +0100

    Available scripts now greet the wholesome person who shared his idea

--- moth.sh
+++ moth.sh
@@ -4,14 +4,17 @@ case "$#" in
 [1]*)
     idea=$"|"$1"|General|Anonymous|"
     echo "$idea">>ideas.md&echo "$idea">>README.md
+    echo -e "Your idea has been registered!\nThank you for your contribution you can now pull request it."
     ;;
 [2]*)
     idea=$"|"$1"|$2|Anonymous|"
     echo "$idea">>ideas.md&echo "$idea">>README.md
+    echo -e "Your idea has been registered!\nThank you for your contribution you can now pull request it."
     ;;
 [3]*)
     idea=$"|"$1"|"$2"|"$3"|"
     echo "$idea">>ideas.md&echo "$idea">>README.md
+    echo -e "Your idea has been registered!\nThank you for your contribution you can now pull request it."
     ;;
 *)
     echo "Your idea seems to have a trouble!"
